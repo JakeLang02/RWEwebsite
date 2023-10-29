774b5a5cd1f02c60576207c4d3b59f662d480e83
JakeLang02 <jacoblang@ufl.edu>
2023-10-29 16:20:53 -0400

    added team list

---
+++ src/helpers/TeamList.js
@@ -0,0 +1,80 @@
+import Micheal from '../../images/RWE_Headshots/MichaelSHeadshot1.JPG'
+import Aaron from '../../images/RWE_Headshots/AaronSchumer.JPG'
+import AJ from '../../images/RWE_Headshots/AJSawmiller.JPG'
+import Braxton from '../../images/RWE_Headshots/BraxtonEisel.JPG'
+import Gage from '../../images/RWE_Headshots/GagePollard'
+import Garrett from '../../images/RWE_Headshots/GarrettLeath.JPG'
+import George from '../../images/RWE_Headshots/GeorgeGiannopoulus.JPG'
+import Grant from '../../images/RWE_Headshots/GrantSherman.JPG'
+import Jake from '../../images/RWE_Headshots/JakeLang.JPG'
+import James from '../../images/RWE_Headshots/JamesBautista.JPG'
+import Jason from '../../images/RWE_Headshots/JasonDecker.JPG'
+import Jordan from '../../images/RWE_Headshots/JordanRhymer.JPG'
+import Lucas from '../../images/RWE_Headshots/LucasBradshaw.JPG'
+import Matt from '../../images/RWE_Headshots/MattSzatkowski.JPG'
+import Nick from '../../images/RWE_Headshots/NickFamiglietti.JPG'
+
+
+export const TeamList = [
+    {
+        name: "Micheal Sawarynski",
+        image: Micheal,
+    },
+    {
+        name: "Garrett Leath",
+        image: Garrett,
+    },
+    {
+        name: "Aaron Schumer",
+        image: Aaron,
+    },
+    {
+        name: "AJ Sawmiller",
+        image: AJ,
+    },
+    {
+        name: "Braxton Eisel",
+        image: Braxton,
+    },
+    {
+        name: "Gage Pollard",
+        image: Gage,
+    },
+    {
+        name: "George Giannopoulus",
+        image: George,
+    },
+    {
+        name: "Grant Sherman",
+        image: Grant,
+    },
+    {
+        name: "Jake Lang",
+        image: Jake,
+    },
+    {
+        name: "James Bautista",
+        image: James,
+    },
+    {
+        name: "Jason Decker",
+        image: Jason,
+    },
+    {
+        name: "Jordan Rhymer",
+        image: Jordan,
+    },
+    {
+        name: "Lucas Bradshaw",
+        image: Lucas,
+    },
+    {
+        name: "Matt Szatkowski",
+        image: Matt,
+    },
+    {
+        name: "Nick Famiglietti",
+        image: Nick,
+    },
+    
+];
