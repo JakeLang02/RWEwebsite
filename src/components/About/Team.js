197a9877212079b898421c2514465c8a38b936ba
msawarynski <92892520+msawarynski@users.noreply.github.com>
2023-11-02 14:05:34 -0400

    added team photo

--- src/components/About/Team.js
+++ src/components/About/Team.js
@@ -4,11 +4,12 @@ import { SDList } from '../../helpers/SDList';
 import { BusinessList } from '../../helpers/BusinessList';
 import TeamMember from '../../helpers/TeamMember';
 import './Team.css';
+import groupPhoto from '../../images/RWETeam.JPG';
 
 const Team = () => {
     const renderTeamList = (list, title) => (
         <>
-            <h1 className="TeamTitle">{title}</h1>
+            <h1 className="TeamTitle mx-auto mt-28">{title}</h1>
             <div className="TeamList">
                 {list.map((teamMember, key) => (
                     <TeamMember
@@ -21,13 +22,21 @@ const Team = () => {
         </>
     );
 
+    
     return (
-        <div className="Team">
-            {renderTeamList(ExecList, 'Executive Board')}
-            {renderTeamList(SDList, 'Solar Drone Team')}
-            {renderTeamList(BusinessList, 'Business Board')}
-        </div>
-    );
+      <div className="OuterContainer mx-auto mt-10">
+          <div className="TeamHeader">
+              <img src={groupPhoto} alt="Team Group Photo" className="TeamHeaderImage" />
+              <div className="TeamHeaderContent">
+                  <h1>Our Team</h1>
+              </div>
+          </div>
+          <div className="Team">
+              {renderTeamList(ExecList, 'Executive Board')}
+              {renderTeamList(SDList, 'Solar Drone Team')}
+              {renderTeamList(BusinessList, 'Business Board')}
+          </div>
+      </div>
+  );
 };
-
 export default Team;
