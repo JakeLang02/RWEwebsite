800ee2c452bcbb08e561eca7fabc36c20efef18e
JakeLang02 <jacoblang@ufl.edu>
2023-11-07 11:18:58 -0500

    navbar huge update

---
+++ src/components/Footer/footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./footer.css";
+
+function Footer() {
+  return (
+    <div className="container-fluid no-padding py-2 px-4 shadow-md text-white relative bottom-0 left-0 w-full ">
+      <div className="content">
+        <p> &copy; 2023 realworldengineering.com</p>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,21 +2,19 @@ import './Navbar.css';
 import { Link } from 'react-router-dom';
 import RWELogo from '../../images/RWENiceLogo.png'
 
-const Navbar = () => {
+function Navbar(){
   return (
-    <nav className="bg-gradient-to-r from-stone-900 to-stone-900 py-2 px-4 shadow-md text-white fixed top-0 left-0 w-full z-50">
-      <div className="container mx-auto flex justify-between items-center">
-        <div className="flex items-center">
-          <img src={RWELogo} id="logo" alt="RWE Logo"className="h-24 w-27 mr-5"/> {/* further reduce the image dimensions */}
-        </div>
-        <ul className="flex space-x-6">
-          <li><Link to="/" className="hover:text-gray-300 hover:underline text-base font-semibold">Home</Link></li>
-          <li><Link to="/team" className="hover:text-gray-300 hover:underline text-base font-semibold">Team</Link></li>
-          <li><Link to="/support" className="hover:text-gray-300 hover:underline text-base font-semibold">Support</Link></li>
-          <li><Link to="/contact" className="hover:text-gray-300 hover:underline text-base font-semibold">Contact</Link></li>
-        </ul>
+    <div className="navbar bg-gradient-to-r from-stone-900 to-stone-900 py-2 px-4 shadow-md text-white fixed top-0 left-0 w-full z-50">
+      <div className="leftSide">
+        <img src={RWELogo} id="logo" alt="RWE Logo"className="h-24 w-27 mr-5"/> {/* further reduce the image dimensions */}
       </div>
-    </nav>
+      <div className="rightSide">
+          <Link to="/" className="hover:text-gray-300 hover:underline text-base font-semibold">Home</Link>
+          <Link to="/team" className="hover:text-gray-300 hover:underline text-base font-semibold">Team</Link>
+          <Link to="/support" className="hover:text-gray-300 hover:underline text-base font-semibold">Support</Link>
+          <Link to="/contact" className="hover:text-gray-300 hover:underline text-base font-semibold">Contact</Link>
+      </div>
+    </div>
   );
 }
 
